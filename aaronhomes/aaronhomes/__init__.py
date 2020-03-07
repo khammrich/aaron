82f9c7969971243eb6cd1151b78829a6635f6af9
Kyle <kyle@temperedlab.com>
2020-03-07 00:05:10 -0800

    first commit

---
+++ aaronhomes/aaronhomes/__init__.py
@@ -0,0 +1,8 @@
+"""
+The flask application package.
+"""
+#import React 
+from flask import Flask
+app = Flask(__name__)
+
+import aaronhomes.views
---
+++ aaronhomes/aaronhomes/module1.py
@@ -0,0 +1,7 @@
+from React import react
+
+export default class Checkbox extends React.Component {
+  render() {
+      return (<div><input type="checkbox" /></div>)
+  }
+}
